01588198d321146688a229e103947ede75bdc2f4
Shubham <shubhampareek4000@gmail.com>
2020-03-06 01:24:22 +0530

    Initial commit

---
+++ Perceptron.py
@@ -0,0 +1,36 @@
+import csv
+import numpy as np
+
+def activation_function():
+    print("b")
+
+def perceptron(inputs, weights, bias):
+    sum = bias
+    for i in inputs:
+        sum += weights[i]*inputs[i]
+    output = activation_function(sum)
+    return output
+
+def training(inputs, target, lr):
+    # setting initial weights randomly
+    weights = np.random.rand(1,4)
+    output = perceptron(inputs, weights, -1)
+    error = target - output
+    for w in weights:
+        w += error*inputs[i]*lr
+
+
+# importing data
+with open ('train.data', 'r') as f:
+    train = list(csv.reader(f,delimiter=','))
+
+with open ('test.data', 'r') as f:
+    test = list(csv.reader(f,delimiter=','))
+
+train = np.array(train, dtype=None)
+test = np.array(test, dtype=None)
+
+
+train(inputs, target, 0.1)
+
+lr = 3
