11b23cc69df9b39eda3413260c1b53325adb6750
Shubham <shubhampareek4000@gmail.com>
2020-03-13 00:37:54 +0530

    changes to the training function

--- Perceptron.py
+++ Perceptron.py
@@ -7,7 +7,7 @@ import numpy as np
     The perceptron function is the one containing the actual perceptron algorithm. The output of which
     depends on the activation function.
     # TODO: Normalize our data so all the data is within the range 0 to 1
-            Convert our output to an integer ?
+            Convert our output to an integer
             (maybe if output == 0, then print(class 0) and if output == 1 print (class 1))
             Write a proper activation function (gradient descent)
 
@@ -16,9 +16,13 @@ import numpy as np
 
 def perceptron1(inputs, weights, bias):
     sum = bias
+    print("inputs in perceptron1 is " +str(inputs))
+    print("weights in perceptron1 is " +str(weights))
     #len(inputs)-1 because the final element will be the output
     for i in range(len(inputs)-1):
-        sum += weights[i]*inputs[i]
+        sum += weights[i]*2
+        #sum += float(weights[i])*float(inputs[i])
+        #sum += float(weights[i])*float(inputs[i])
     output = activation_function(sum)
     return output
 
@@ -28,17 +32,16 @@ def activation_function(sum):
         return sum
     else : return 0
 
-def training (bias, weights, data, lr, epochs):
-    for epoch in epochs:
-        for row in data:
-            pred = perceptron1(row, weights, 0.2)
-            error = row[-1] - pred
-            bias = bias + (lr * error)
-            for i in range(len(row-1)):
-                weights[i] = weights[i] + (lr * error * row[i])
-            print ("%s epochs %s error", epoch, error)
 
-    return weights
+def training (bias, weights, data, lr, epochs):
+    for epoch in range(epochs):
+        pred = perceptron1(data, weights, 0.2)
+        error = row[-1] - pred
+        bias = bias + (lr * error)
+        for i in range(len(data-1)):
+            weights[i] = weights[i] + (lr * error * data[i])
+        print ("%s epochs %s error", epoch, error)
+
 
 #function to remove class-3 from training and testing set cuz its not needed
 def clean_list(list):
